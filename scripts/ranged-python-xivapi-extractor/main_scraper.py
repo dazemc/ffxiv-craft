7b494ba546ad42a453f2dc5a19c5c10bcf389708
dazemc <daazedjmcfarland@gmail.com>
2023-05-01 17:28:46 -0700

    More multithreading, cannot exceed 20 due to xivapi limits

--- scripts/ranged-python-xivapi-extractor/main_scraper.py
+++ scripts/ranged-python-xivapi-extractor/main_scraper.py
@@ -51,35 +51,55 @@ if __name__ == '__main__':
     recipe_data = r.json()
     pages_amount = recipe_data['Pagination']['PageTotal']
     # ID_range = range(1, pages_amount + 1)  # +1 because of how range works lol
-    # ID_range = range(1, 1 + 1) # +1 because of how range works lol
 
     recipes = defaultdict(list)
 
 
     # Handle the actual API calls here
-    def api_call(start):
-        for ID in tqdm(range(start, pages_amount, 2)):
-            # Construct an URL to get data from for every page
-            url_call = 'https://xivapi.com/Recipe?page={0}&columns=Name_en,Name_de,Name_fr,Name_ja,ClassJob.NameEnglish,DurabilityFactor,QualityFactor,DifficultyFactor,RequiredControl,RequiredCraftsmanship,RecipeLevelTable'.format(
-                ID)
-            r = requests.get(url_call)
-            page_data = r.json()
+    def api_call(ID):
+        # Construct an URL to get data from for every page
+        url_call = 'https://xivapi.com/Recipe?page={0}&columns=Name_en,Name_de,Name_fr,Name_ja,ClassJob.NameEnglish,DurabilityFactor,QualityFactor,DifficultyFactor,RequiredControl,RequiredCraftsmanship,RecipeLevelTable'.format(
+            ID)
+        r = requests.get(url_call)
+        page_data = r.json()
+        if r.status_code == 429:
+            print("Too many requests sent to XIVAPI!")
 
-            # Iterate through each recipe on the page
-            for recipe in page_data['Results']:
-                # Save the data to the recipes dictionary, with a key for each crafting job
-                key = recipe['ClassJob']['NameEnglish']
-                constructed_recipe = construct_recipe_json(recipe)
-                if constructed_recipe:
-                    recipes[key].append(constructed_recipe)
+        # Iterate through each recipe on the page
+        for recipe in page_data['Results']:
+            # Save the data to the recipes dictionary, with a key for each crafting job
+            key = recipe['ClassJob']['NameEnglish']
+            constructed_recipe = construct_recipe_json(recipe)
+            if constructed_recipe:
+                recipes[key].append(constructed_recipe)
 
-    # Threading, could be better but should theoretically cut time in half
-    t1 = threading.Thread(target=api_call, args=(1,))
-    t2 = threading.Thread(target=api_call, args=(2,))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+
+    threads = []
+    for i in range(1, pages_amount + 1):
+        t = threading.Thread(target=api_call, args=(i,))
+        threads.append(t)
+
+
+    def start_threads(slice):
+        if slice % 20 != 0:
+            for k in threads[div_by_rate:div_by_rate + slice]:
+                k.start()
+            for k in threads[div_by_rate:div_by_rate + slice]:
+                k.join()
+        for j in threads[slice - 20:slice]:
+            j.start()
+        for j in threads[slice - 20:slice]:
+            j.join()
+
+
+    # XIVAPI Is rate limited to 20, so we can not exceed 20 threads
+    div_by_rate = pages_amount - (pages_amount % 20)  # Gets amount that is divisible by 20
+    left_over = pages_amount % 20  # Pages left after division
+    groups = round(div_by_rate / 20)  # How many groups we need to send
+    for i in tqdm(range(1, groups + 1)):
+        start_threads(i * 20)
+    if left_over:
+        start_threads(left_over)
 
     # Save the data in recipes to a .json file in the out folder, with a file for every job
     # Create out directory, ignore error if it already exists
